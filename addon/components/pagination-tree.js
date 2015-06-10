f375174019e72a08c2243b0257068d61ee1d1ae5
seymac <seymaccakmak@gmail.com>
2015-06-10 14:35:25 +0300

    adding totalPageNumber property to component and producing page button list from it

--- addon/components/pagination-tree.js
+++ addon/components/pagination-tree.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	totalPageNumber : 1,
+	currentPage: 1,
+
+	pages: Ember.computed( 'totalPageNumber', function(){
+    	var totalPageNumber = this.get('totalPageNumber');
+    	var pages = Ember.A([]);
+    	for(var i=1; i <= totalPageNumber; i++){
+    		pages.push(i)
+    	}
+    	return pages;
+  	})
 });
