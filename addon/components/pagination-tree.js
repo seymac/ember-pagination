d62d7efbfcd65d09f2d30f468c597437690921ea
seymac <seymaccakmak@gmail.com>
2015-06-10 15:21:24 +0300

    adding gotoPage function to change current page

--- addon/components/pagination-tree.js
+++ addon/components/pagination-tree.js
@@ -4,12 +4,29 @@ export default Ember.Component.extend({
 	totalPageNumber : 1,
 	currentPage: 1,
 
-	pages: Ember.computed( 'totalPageNumber', function(){
+	pages: Ember.computed( 'totalPageNumber', 'currentPage', function(){
     	var totalPageNumber = this.get('totalPageNumber');
+    	var currentPage = this.get('currentPage');
     	var pages = Ember.A([]);
     	for(var i=1; i <= totalPageNumber; i++){
-    		pages.push(i)
+    		var page = Ember.Object.create({
+    			number : i,
+    			isCurrentPage: i == currentPage
+    		});
+    		pages.push(page);
     	}
     	return pages;
-  	})
+  	}),
+
+  	actions:{
+  		goToPage: function(pageNumber){
+  			var totalPageNumber = this.get('totalPageNumber');
+  			var pages = this.get('pages');
+  			pages.forEach(function(page){
+  				page.set('isCurrentPage', page.get('number') == pageNumber);
+  			});
+  			this.set('currentPage', pageNumber);
+  		}
+  	}
+
 });
